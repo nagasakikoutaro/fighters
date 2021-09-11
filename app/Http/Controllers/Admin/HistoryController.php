3049ac2315d199067f798fe6228b3caa3905b7bd
長崎宏太朗 <kmny03@gmail.com>
2021-09-11 02:09:08 +0000

    HistoryControllerの編集

--- app/Http/Controllers/Admin/HistoryController.php
+++ app/Http/Controllers/Admin/HistoryController.php
@@ -8,4 +8,20 @@ use App\Http\Controllers\Controller;
 class HistoryController extends Controller
 {
     //
+    public function add()
+    {
+        return view('admin.history.create');
+    }
+    public function create()
+    {
+        return view('admin/history/create');
+    }
+    public function edit()
+    {
+        return view('admin.history.edit');
+    }
+    public function update()
+    {
+        return view('admin/history/edit');
+    }
 }
