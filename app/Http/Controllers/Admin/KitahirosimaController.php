8f9122c40ad048db23500d9149cd3a1376128c13
長崎宏太朗 <kmny03@gmail.com>
2021-09-11 02:12:19 +0000

    kitahirosimacontrollerの編集

--- app/Http/Controllers/Admin/KitahirosimaController.php
+++ app/Http/Controllers/Admin/KitahirosimaController.php
@@ -8,4 +8,20 @@ use App\Http\Controllers\Controller;
 class KitahirosimaController extends Controller
 {
     //
+     public function add()
+    {
+        return view('admin.kitahirosima.create');
+    }
+    public function create()
+    {
+        return view('admin/kitahirosima/create');
+    }
+    public function edit()
+    {
+        return view('admin.kitahirosima.edit');
+    }
+    public function update()
+    {
+        return view('admin/kitahirosima/edit');
+    }
 }
