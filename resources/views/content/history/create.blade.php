2b4ad8d2fbae56e687595460d07dc7ed662540ff
長崎宏太朗 <kmny03@gmail.com>
2021-09-20 05:46:20 +0000

    /fighters/resources/views/content/history/create.blade.phpの編集

--- resources/views/content/history/create.blade.php
+++ resources/views/content/history/create.blade.php
@@ -2,16 +2,16 @@
 @extends('layouts.history')
 
 
-{{-- admin.blade.phpの@yield('title')に'思い出'を埋め込む --}}
+{{-- history.blade.phpの@yield('title')に'思い出'を埋め込む --}}
 @section('title', '思い出')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- history.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>思い出</h2>
-                 <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+                 <form action="{{ action('Content\HistoryController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
