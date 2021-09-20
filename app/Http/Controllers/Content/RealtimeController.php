d96b3d1e7f341773adca1f95d69ce8362692227e
長崎宏太朗 <kmny03@gmail.com>
2021-09-20 03:31:21 +0000

    realtimecontroller,viewの変更

--- app/Http/Controllers/Content/RealtimeController.php
+++ app/Http/Controllers/Content/RealtimeController.php
@@ -10,18 +10,18 @@ class RealtimeController extends Controller
     //
      public function add()
     {
-        return view('admin.realtime.create');
+        return view('content.realtime.create');
     }
     public function create()
     {
-        return view('admin/realtime/create');
+        return view('content/realtime/create');
     }
     public function edit()
     {
-        return view('admin.realtime.edit');
+        return view('content.realtime.edit');
     }
     public function update()
     {
-        return view('admin/realtime/edit');
+        return view('content/realtime/edit');
     }
 }
