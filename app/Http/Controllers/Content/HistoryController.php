3eae05acb15443309d0db0baa765577eca4527fc
長崎宏太朗 <kmny03@gmail.com>
2021-09-21 01:31:35 +0000

    history,kitahirosimacontrollerの編集

--- app/Http/Controllers/Content/HistoryController.php
+++ app/Http/Controllers/Content/HistoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Content;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+
+use App\History;
 class HistoryController extends Controller
 {
     //
--- app/Http/Controllers/Content/KitahirosimaController.php
+++ app/Http/Controllers/Content/KitahirosimaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Content;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Kitahirosima;
 class KitahirosimaController extends Controller
 {
     //
