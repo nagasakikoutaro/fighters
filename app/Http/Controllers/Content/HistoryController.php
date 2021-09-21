02e2650bfe926eb451e958dc7df99b2d2e5ee43f
長崎宏太朗 <kmny03@gmail.com>
2021-09-21 04:38:39 +0000

    /fighters/app/Http/Controllers/Content/HistoryController.php,/fighters/app/Http/Controllers/Content/KitahirosimaController.phpのcreateaction編集、indexaction追加

--- app/Http/Controllers/Content/HistoryController.php
+++ app/Http/Controllers/Content/HistoryController.php
@@ -16,7 +16,39 @@ class HistoryController extends Controller
     }
     public function create(Request $request)
     {
+        //validationを行う
+        $this->validation($request, History::$rules);
+        $history = new History();
+        $form = $request->all();
+        
+        //formに画像があれば、保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $history->image_path = basename($path);
+        } else {
+            $history->image_path = null;
+        }    
+        
+        unset($form['_token']);
+        unset($form['image']);
+        //データベースに保存
+        $history->fill($form);
+        $history->save();
+        
         return redirect('content/history/create');
+    }
+    
+    public function index(Request $request)
+    {
+        $cond_title = $request->$cond_title;
+        if ($cond_title != '') {
+            //検索されたら検索結果を取得する
+            $posts = History::where('title', $cond_title)->get();
+        } else {
+            //それ以外はすべてのヒストリーを取得する
+            $posts = History::all();
+        }
+        return view('content.history.index',['posts' => $posts, 'cond_title' => $cond_title]);
     }
     public function edit()
     {
--- app/Http/Controllers/Content/KitahirosimaController.php
+++ app/Http/Controllers/Content/KitahirosimaController.php
@@ -15,7 +15,38 @@ class KitahirosimaController extends Controller
     }
     public function create(Request $request)
     {
+        //validationを行う
+        $this->validate($request, Kitahirosima::$rules);
+        $kitahirosima = new Kitahirosima();
+        $form = $request->all();
+        
+        //formに画像があれば保存する
+        if(isset($form['image'])){
+            $path = $request->file('image')->store('public/image');
+            $kitahirosima->image_path = basename($path);
+        } else {
+            $kitahirosima->image_path = null;
+        }
+        
+        unset($form['_token']);
+        unset($form['image']);
+        //データベースに保存
+        $kitahirosima->fill($form);
+        $kitahirosima->save();
         return redirect('content/kitahirosima/create');
+    }
+    
+    public function index(Request $request)
+    {
+        $cond_title = $request->$cond_title;
+        if ($cond_title != '') {
+            //検索されたら検索結果を習得する
+            $posts = Kitahirosima::where('title', $cond_title)->get();
+        }else{
+            //それ以外はすべての情報を習得する
+            $posts = Kitahirosima::all();
+        }
+        return view('content.kitahirosima.index',['posts' => $posts, 'cond_title' => $cond_title]);
     }
     public function edit()
     {
