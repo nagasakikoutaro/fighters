0e5eb375824a7e3e576804868e209c5146e5f143
長崎宏太朗 <kmny03@gmail.com>
2021-09-21 00:49:15 +0000

    /fighters/database/migrations/2021_09_21_003350_create_history_table.php作成

---
+++ database/migrations/2021_09_21_003350_create_history_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');//タイトルを保存
+            $table->string('body');//本文を保存
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history');
+    }
+}
