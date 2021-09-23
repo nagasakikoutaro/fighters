0cf6f8f189823a263270fda48405bf38afe0d01f
長崎宏太朗 <kmny03@gmail.com>
2021-09-23 01:24:08 +0000

    /fighters/resources/views/content/history/create.blade.php,title変更

--- resources/views/content/history/create.blade.php
+++ resources/views/content/history/create.blade.php
@@ -3,14 +3,14 @@
 
 
 {{-- history.blade.phpの@yield('title')に'思い出'を埋め込む --}}
-@section('title', '思い出')
+@section('title', '選手名')
 
 {{-- history.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>思い出</h2>
+                <h2>選手名</h2>
                  <form action="{{ action('Content\HistoryController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
@@ -21,13 +21,7 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">本文</label>
+                        <label class="col-md-2">選出理由</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
