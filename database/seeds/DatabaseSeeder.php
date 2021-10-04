b62f6bfe74ec1af06ed53c413051c94e239b3791
長崎宏太朗 <kmny03@gmail.com>
2021-10-04 08:33:16 +0000

    /fighters/database/seeds/DatabaseSeeder.phphe編集

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(PlayersTableSeeder::class);
+       $this->call([
+          PlayersTableSeeder::class,
+        ]);
     }
 }
